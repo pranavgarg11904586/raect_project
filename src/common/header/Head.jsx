6e1657ea0e491611972f7056cc413f18fe585ad0
pranavgarg11904586 <pranavg11904586@gmail.com>
2022-05-06 18:03:55 +0530

    first commit

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> +91 9024633578</label>
+            <i className='fa fa-envelope'></i>
+            <label> pranavg855@gmail.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+            <span>🚩</span>
+            <label>EN</label>
+            <span>🚩</span>
+            <label>INR</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
